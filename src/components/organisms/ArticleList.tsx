74092ab09236c37c2d3949c598374c322d5acc87
dqn <dqn443@gmail.com>
2020-11-27 23:03:50 +0900

    chore: modify color

--- src/components/organisms/ArticleList.tsx
+++ src/components/organisms/ArticleList.tsx
@@ -10,14 +10,14 @@ type Props = {
 
 export const ArticleList: React.FC<Props> = ({ articles }) => {
   return (
-    <div className="flex flex-wrap divide-y divide-gray-700">
+    <div className="flex flex-wrap divide-y divide-gray-600">
       {articles.map(({ id, title, published }, i) => (
         <article className="w-full py-5" key={i}>
           <Link href={"/articles/" + id}>
             <a className="text-lg font-bold hover:underline">{title}</a>
           </Link>
           <div>
-            <time className="text-sm text-gray-500">
+            <time className="text-sm text-gray-400">
               {dayjs(published).format("YYYY/MM/DD")}
             </time>
           </div>
