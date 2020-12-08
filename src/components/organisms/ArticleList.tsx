f59e42e7cc23236ce3bf9a47c79a90451703f775
dqn <dqn443@gmail.com>
2020-12-09 05:10:16 +0900

    feat: add excerpt in article list

--- src/components/organisms/ArticleList.tsx
+++ src/components/organisms/ArticleList.tsx
@@ -11,8 +11,8 @@ type Props = {
 export const ArticleList: React.FC<Props> = ({ articles }) => {
   return (
     <div className="flex flex-wrap divide-y divide-gray-600">
-      {articles.map(({ id, title, published }, i) => (
-        <article className="w-full py-5" key={i}>
+      {articles.map(({ id, title, published, excerpt }, i) => (
+        <article className="w-full py-6" key={i}>
           <Link href={"/articles/" + id}>
             <a className="text-lg font-bold hover:underline">{title}</a>
           </Link>
@@ -21,6 +21,7 @@ export const ArticleList: React.FC<Props> = ({ articles }) => {
               {dayjs(published).format("YYYY/MM/DD")}
             </time>
           </div>
+          <p className="truncate text-gray-400 mt-4 text-sm">{excerpt}</p>
         </article>
       ))}
     </div>
