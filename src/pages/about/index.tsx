241f5b7ca637cd1825b7aa87b2559b4b03a8e702
dqn <dqn270@gmail.com>
2021-05-02 19:15:51 +0900

    fix: fix build error

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,11 +3,11 @@ import { NextSeo } from "next-seo";
 
 import { About } from "@/components/pages/About";
 
-const AboutPage: NextPage = (props) => {
+const AboutPage: NextPage = () => {
   return (
     <>
       <NextSeo title="About" description="このブログの管理者について。" />
-      <About {...props} />
+      <About />
     </>
   );
 };
